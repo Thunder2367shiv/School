08857f058609c7b4137721e39794603b5864e319
thunder2367shiv <2022kuec2034@iiitkota.ac.in>
2025-04-23 03:57:47 +0530

    last commit

--- Config/db.js
+++ Config/db.js
@@ -2,13 +2,13 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+ 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306 // Default to 3306 if DB_PORT is not set
+  port: process.env.DB_PORT 
 });
 
 db.connect(err => {
